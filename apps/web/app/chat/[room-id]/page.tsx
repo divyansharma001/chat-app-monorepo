82aa9213e3d44178568b9d814379e96406d871da
Divyansh Sharma <divyanshsharma4507@gmail.com>
2025-01-15 00:28:03 +0530

    Enhance ChatPage layout and integrate TextInput component

--- apps/web/app/chat/[room-id]/page.tsx
+++ apps/web/app/chat/[room-id]/page.tsx
@@ -1,7 +1,23 @@
+import { TextInput } from "@repo/ui/text-input";
+
 export default function ChatPage() {
   return (
     <>
-      <div>chat-app</div>
+      <div style={{
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "black",
+        display: "flex",
+        justifyContent: "space-between",
+        justifyItems: "center",
+        flexDirection: "column"
+      }}>
+        <div>chat-app</div>
+        <div>
+        <TextInput placeholder="Type here.."></TextInput>
+        </div>
+        
+    </div>
     </>
   );
 }
